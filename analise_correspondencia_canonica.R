7a641bd37bec2fb81aa3e80422ced00244dc92e2
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-03-01 23:26:52 -0300

    Add files via upload

---
+++ analise_correspondencia_canonica.R
@@ -0,0 +1,19 @@
+
+# Análise Multivariada - Análise de Correspondência Canônica (CCA) -------------------------------------------------------------------------
+# Autoria do script: Jeanne Franco ---------------------------------------------------------------------------------------------------------
+# Data: 01/03/2022 -------------------------------------------------------------------------------------------------------------------------
+
+# Conceito da Análise de Correspondência Canônica ------------------------------------------------------------------------------------------
+
+## O objetivo dessa análise é identificar a influência de variáveis ambientais sobre os padrões
+## de composição e abundância das espécies numa comunidade.
+
+## São necessárias duas matriz, uma matriz x com variáveis independentes e uma matriz y com
+## variáveis dependentes.
+
+## A diferença dessa análise para a análise de RDA é que esta assume respostas lineares e a CCA
+## assume respostas não-lineares (formas curvas ou unimodais).
+
+## As correlações da CCA tenta maximizar as relações entre as variáveis x e y.
+
+
